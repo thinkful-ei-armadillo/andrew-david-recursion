8b22a5223f8a566ebd67c94a30096ab1a86422da
David Haugen <haugen.dj@gmail.com>
2019-04-01 11:14:53 -0500

    add second drill

--- andrew-david-recursion.js
+++ andrew-david-recursion.js
@@ -13,4 +13,20 @@ function countSheep(num) {
   console.log(num, ": Another sheep jump over the fence");
   return countSheep(num - 1);
 }
-countSheep(3);
+
+function powerCalculator(int, exp){
+  // What is the input to the program? The program takes an integer and an exponent as parameters. 
+  // What is the output of the program? A number equal to the integer raised to the power of our exponent parameter. 
+  // What is the input to each recursive call? The input for each recursive call will include the integer and the exponent, which will decrement with each call. 
+  // What is the output of each recursive call? Each recursive call will return the integer multiplied by another call for our function.
+
+  // integer * recursive call; 
+
+  // base case:
+  if(!exp){
+    return 1;
+  } 
+
+  // recursive case
+  return int * powerCalculator(int, exp -1)
+}
